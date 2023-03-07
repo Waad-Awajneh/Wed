816e0791f9da1cd7f49eac8aeb4d91a00ad2eec2
unknown <waad.awajneh707@gmail.com>
2023-03-07 10:34:41 +0300

    final route component

--- React-Master/src/routes/routes.jsx
+++ React-Master/src/routes/routes.jsx
@@ -1,23 +1,10 @@
+// static imports
 import { lazy, Suspense } from "react";
-import { useIsAuthenticated } from "react-auth-kit";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import App from "../App";
-// import AddPost from "../components/addPost";
 import Home from "../components/Home";
-// import Login from "../components/login";
 import FormAskPriceModal from "../components/Modal/FormAskPriceModal";
 import FormConnectModal from "../components/Modal/FormConnectModal";
-// import SignUp from "../components/signUp";
-// import SinglePost from "../components/singlePost";
-// import About from "../views/about";
-// import Contact from "../views/contact";
-// import Favorite from "../views/Favorite";
-// import Landing from "../views/landing";
-// import { NotFound } from "../views/notFoundPage";
-// import Profile from "../views/Profile";
-// import Search from "../views/Search";
-// import UserProfile from "../views/UserProfile";
-
+// dynamic imports
 const App = lazy(() => import("../App"));
 const Profile = lazy(() => import("../views/Profile"));
 const UserProfile = lazy(() => import("../views/UserProfile"));
@@ -33,8 +20,6 @@ const SignUp = lazy(() => import("../components/signUp"));
 const NotFound = lazy(() => import("../views/notFoundPage"));
 
 export const AllRoutes = (params) => {
-  const isAuthenticated = useIsAuthenticated();
-
   return (
     <BrowserRouter>
       <FormConnectModal />
@@ -42,22 +27,22 @@ export const AllRoutes = (params) => {
 
       <Suspense fallback={"Loading ....."}>
         <Routes>
-          <Route exact path="/" element={<App />}>
-            <Route exact index element={<Home openTap={1} />} />
-            <Route exact path="Home" element={<Home openTap={1} />} />
-            <Route exact path="follow" element={<Home openTap={2} />} />
+          <Route path="/" element={<App />}>
+            <Route index element={<Home openTap={1} />} />
+            <Route path="Home" element={<Home openTap={1} />} />
+            <Route path="follow" element={<Home openTap={2} />} />
           </Route>
-          <Route exact path="/profile" element={<Profile />} />
-          <Route exact path="/profile/:id" element={<UserProfile />} />
-          <Route exact path="/contact" element={<Contact />} />
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/about" element={<About />} />
-          <Route exact path="/Landing" element={<Landing />} />
-          <Route exact path="/AddPost" element={<AddPost />} />
-          <Route exact path="/Favorite" element={<Favorite />} />
-          <Route exact path="/search" element={<Search />} />
-          <Route exact path="/SinglePost/:id" element={<SinglePost />} />
-          <Route exact path="/SignUp" element={<SignUp />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile/:id" element={<UserProfile />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/Landing" element={<Landing />} />
+          <Route path="/AddPost" element={<AddPost />} />
+          <Route path="/Favorite" element={<Favorite />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/SinglePost/:id" element={<SinglePost />} />
+          <Route path="/SignUp" element={<SignUp />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Suspense>
